7c0eba1ec5beb288ef6ace9ba00d34072ae33db2
Rodrigo Martínez Juárez <A01351051@itesm.mx>
2022-07-28 05:36:03 -0500

    Materias

--- src/Components/Materia1/index.jsx
+++ src/Components/Materia1/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const Header = () => {
+  return (
+    <div className="portada">
+      <div className="portada_container">
+      <h1>Quimica</h1>
+      </div>
+    </div>
+  );
+};
 const index = () => {
-  return <div>Materia1</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default index;
